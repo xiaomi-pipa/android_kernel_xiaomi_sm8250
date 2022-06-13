a2d566aa9916ded388a06463ba7b40d89daaaa5c
SeongJae Park <sj@kernel.org>
2022-06-13 19:22:58 +0000

    mm/damon/schemes: add 'LRU_DEPRIO' action
    This commit adds a new DAMON-based operation scheme action called
    'LRU_DEPRIO' for physical address space.  The action deprioritizes pages
    in the memory area of the target access pattern on their LRU lists.  This
    is hence supposed to be used for rarely accessed (cold) memory regions so
    that cold pages could be more likely reclaimed first under memory
    pressure.  Internally, it simply calls 'lru_deactivate()'.
    
    Using this with 'LRU_PRIO' action for hot pages, users can proactively
    sort LRU lists based on the access pattern.  That is, it can make the LRU
    lists somewhat more trustworthy source of access temperature.  As a
    result, efficiency of LRU-lists based mechanisms including the reclamation
    target selection could be improved.
    
    Link: https://lkml.kernel.org/r/20220613192301.8817-7-sj@kernel.org
    Signed-off-by: SeongJae Park <sj@kernel.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -84,6 +84,7 @@ struct damon_target {
  * @DAMOS_HUGEPAGE:	Call ``madvise()`` for the region with MADV_HUGEPAGE.
  * @DAMOS_NOHUGEPAGE:	Call ``madvise()`` for the region with MADV_NOHUGEPAGE.
  * @DAMOS_LRU_PRIO:	Prioritize the region on its LRU lists.
+ * @DAMOS_LRU_DEPRIO:	Deprioritize the region on its LRU lists.
  * @DAMOS_STAT:		Do nothing but count the stat.
  */
 enum damos_action {
@@ -93,6 +94,7 @@ enum damos_action {
 	DAMOS_HUGEPAGE,
 	DAMOS_NOHUGEPAGE,
 	DAMOS_LRU_PRIO,
+	DAMOS_LRU_DEPRIO,
 	DAMOS_STAT,		/* Do nothing but only record the stat */
 };
 
