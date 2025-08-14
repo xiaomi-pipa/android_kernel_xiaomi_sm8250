8d0d451373461a0ab3740785a1f5533ea6ad8d97
TIMISONG-dev <dima.pilaer@gmail.com>
2025-08-14 23:14:53 +0300

    mm/compaction: Use READ_ONCE(pgdat->kswapd->__state)

--- mm/compaction.c
+++ mm/compaction.c
@@ -1888,7 +1888,7 @@ static inline bool is_via_compact_memory(int order)
 
 static bool kswapd_is_running(pg_data_t *pgdat)
 {
-	return pgdat->kswapd && (pgdat->kswapd->state == TASK_RUNNING);
+	return pgdat->kswapd && (READ_ONCE(pgdat->kswapd->__state) == TASK_RUNNING);
 }
 
 /*
